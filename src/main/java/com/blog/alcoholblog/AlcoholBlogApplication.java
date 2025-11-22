6996cf36fdb59f2b707435e3f0ef18acd99c7698
SVidak <svidakofficial@gmail.com>
2025-11-22 18:27:37 +0100

    Initial commit

---
+++ src/main/java/com/blog/alcoholblog/AlcoholBlogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.alcoholblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlcoholBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/alcoholblog/AlcoholBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.alcoholblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
